59f6ca20da82f50802a7523f465d18f2d3d5a282
seffers122 <eseth51@yahoo.com>
2014-10-29 12:07:03 -0600

    .

---
+++ src/main/java/com/seffers122/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,28 @@
+package com.seffers122.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid="LetsModReboot", name="Lets Mod Reboot", version="1.0")
+public class LetsModReboot 
+{
+	@Mod.Instance("LetsModReboot")
+	public static LetsModReboot instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	}
