84f62b0be7249ce7c528c54e2ba0d85327afa30e
seffers122 <eseth51@yahoo.com>
2014-10-29 14:31:13 -0600

    Changes to config handler

--- src/main/java/com/seffers122/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/seffers122/letsmodreboot/LetsModReboot.java
@@ -1,21 +1,25 @@
 package com.seffers122.letsmodreboot;
+import com.seffers122.letsmodreboot.config.configHandler;
 import com.seffers122.letsmodreboot.proxy.IProxy;
+import com.seffers122.letsmodreboot.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="LetsModReboot", name="Lets Mod Reboot", version="1.0")
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class LetsModReboot 
 {
-	@Mod.Instance("LetsModReboot")
+	@Mod.Instance(Reference.MOD_ID)
 	public static LetsModReboot instance;
-	@SidedProxy()
+	@SidedProxy(clientSide=Reference.CLIENT_PROXY_CLASS, serverSide=Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
+	
 	@Mod.EventHandler
-	public void preInit(FMLPostInitializationEvent event)
+	public void preInit(FMLPreInitializationEvent event)
 	{
-		
+		configHandler.init(event.getSuggestedConfigurationFile());
 	}
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)
